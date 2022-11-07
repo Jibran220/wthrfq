5085637b31aabff65e2708c3d3bbd0248babfcb7
Jibran220 <jibranjibran220@gmail.com>
2022-11-06 18:37:49 -0800

    first commit

---
+++ server/controllers/sendEmail.js
@@ -0,0 +1,45 @@
+import nodemailer from "nodemailer";
+export const sendEmail = async (
+  subjecta,
+  message,
+  send_to,
+  sent_from,
+  reply_to,
+  filename,
+  filepath,
+) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "muhammadjibran890@gmail.com", // generated ethereal user
+      pass: "srffpnbdmmgfpmgj", // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const options = {
+    from: sent_from,
+    to: send_to,
+    replyTo: reply_to,
+    subject: subjecta,
+    html: message,
+    attachments: [
+      {
+        // filename: filename,
+          path:`public/images/${filename}`,
+         
+      }
+  ]
+  };
+
+  // Send Email
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
